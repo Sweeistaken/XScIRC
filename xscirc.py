9e89e19bc4399fb29fb1e9436f5315df468a981f
Swee <sweeistaken@gmail.com>
2024-10-19 12:27:00 -0700

    Update xscirc.py

--- xscirc.py
+++ xscirc.py
@@ -1,12 +1,17 @@
-splash="""
+splash, dsplash="""
+░█░█░█▀▀░█▀▀░▀█▀░█▀▄░█▀▀
+░▄▀▄░▀▀█░█░░░░█░░█▀▄░█░░
+░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀▀▀
+""", """
 ░█░█░█▀▀░█▀▀░▀█▀░█▀▄░█▀▀
 ░▄▀▄░▀▀█░█░░░░█░░█▀▄░█░░
 ░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀▀▀
 """
 print(splash)
 from time import sleep
-import sys
 print("Importing sys...")
+import sys
+import traceback
 import os
 print("Importing pygame...")
 import pygame
@@ -18,6 +23,16 @@ print("Importing SugarCaneParse IRC...")
 import scparseirc
 print("Initializing PyGame...")
 pygame.init()
+def tprint(message):
+  global splash
+  print("[" + threading.current_thread().name + "] " + message)
+  splash += "\n[" + threading.current_thread().name + "] " + message
+def handle_error():
+  global loading
+  loading = True
+  tprint("Something went wrong...")
+  for i in traceback.format_exc().rstrip().split("\n"):
+     tprint(i)
 if not pygame.get_init():
   mb.showerror("Error", "Couldn't load all pygame modules!")
   raise SystemError("Aborted...")
@@ -29,6 +44,10 @@ font_debug = pygame.font.SysFont('monospace', 14)
 splash2 = []
 splashRect = []
 def get_splashtext():
+  global splash2
+  global splashRect
+  splash2 = []
+  splashRect = []
   y = 0
   for i in splash.split("\n"):
     j = font_debug.render(i, True, white)
@@ -41,24 +60,33 @@ pygame.display.set_icon(icon)
 run=True
 loading=True
 def ircloop():
-   print("IRC loop placeholder")
-print("Starting IRC thread...")
+   tprint("Mainloop started.")
+   while run:
+      sleep(5)
+      tprint("Ping")
+      pass
+   tprint("Mainloop ended.")
+tprint("Starting IRC thread...")
 ircthread = threading.Thread(target=ircloop)
 ircthread.start()
-print("Reached mainloop!")
+tprint("Reached mainloop!")
+try:
+  raise SystemError("e")
+except:
+  handle_error()
 while run:  
-    if loading:
-      display.fill((0, 0, 0))
-      get_splashtext()
-      for i, j in zip(splash2, splashRect):
-        display.blit(i, j)
-      #text = font_debug.render(i, True, white)
-
-    for event in pygame.event.get():  
-        if event.type == pygame.QUIT:  
-           print("Recieved quit trigger from OS.")
-           run=False
-        pygame.display.update()
-
+  if loading:
+    display.fill([0, 0, 0])
+    get_splashtext()
+    for i, j in zip(splash2, splashRect):
+      display.blit(i, j)
+    pygame.display.flip()
+    #text = font_debug.render(i, True, white)
+  for event in pygame.event.get():  
+      if event.type == pygame.QUIT:  
+          print("Recieved quit trigger from OS.")
+          run=False
+    
+print("Mainloop ended, destroying pygame window.")
 pygame.display.quit()
 print("Exited successfully")
